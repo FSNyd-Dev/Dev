5096341d99b01154a093b08966b5396cb6bd9d87
FSNyd-Dev <fsndudehd@naver.com>
2024-11-06 16:14:12 +0900

    수정 : 캐릭터 움직임 8방향 화 작업 및 애니메이션 추가

--- Gunz/Assets/Script/Player.cs
+++ Gunz/Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -7,8 +8,7 @@ public class Player : MonoBehaviour
 {
     public Animator playerAnim;
     public Rigidbody playerrRigid;
-    public float walkSpeed, walkBackSpeed, oldWalkSpeed, runSpeed, rotateSpeed;
-    public bool isWalking;
+    public float walkSpeed;
     public Transform playerTrans;
 
     void Start()
@@ -19,14 +19,43 @@ public class Player : MonoBehaviour
     // 물리처리 연산을 위해 주기적으로 호출 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
-            playerrRigid.velocity = transform.forward * walkSpeed*Time.deltaTime;
+            playerrRigid.velocity = (transform.forward + -transform.right).normalized * walkSpeed * Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
         {
-            playerrRigid.velocity = -transform.forward * walkBackSpeed * Time.deltaTime;
+            playerrRigid.velocity = (transform.forward+ transform.right).normalized * walkSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            playerrRigid.velocity = (-transform.forward + -transform.right).normalized * walkSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            playerrRigid.velocity = (-transform.forward + transform.right).normalized * walkSpeed * Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                playerrRigid.velocity = transform.forward * walkSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                playerrRigid.velocity = -transform.forward * walkSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                playerrRigid.velocity = transform.right * walkSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                playerrRigid.velocity = -transform.right * walkSpeed * Time.deltaTime;
+            }
         }
     }
 
@@ -35,52 +64,78 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            playerAnim.SetTrigger("walk");
+            playerAnim.SetTrigger("forward");
             playerAnim.ResetTrigger("idle");
-            isWalking = true;
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            playerAnim.ResetTrigger("walk");
-            playerAnim.SetTrigger("idle");
-            isWalking = false;
+            playerAnim.ResetTrigger("forward");
+
+            if(!Input.anyKey)
+                playerAnim.SetTrigger("idle");
+            else
+            {
+                playerAnim.ResetTrigger("idle");
+
+                if(Input.GetKey(KeyCode.A))
+                    playerAnim.SetTrigger("left");
+
+                if(Input.GetKey(KeyCode.D))
+                    playerAnim.SetTrigger("right");
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            playerAnim.SetTrigger("walkback");
+            playerAnim.SetTrigger("back");
             playerAnim.ResetTrigger("idle");
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
-            playerAnim.ResetTrigger("walkback");
-            playerAnim.SetTrigger("idle");
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            playerTrans.Rotate(0, -rotateSpeed*Time.deltaTime,0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            playerTrans.Rotate(0, rotateSpeed * Time.deltaTime, 0);
-        }
-
-        if(isWalking)
-        {
-            if(Input.GetKeyDown(KeyCode.LeftShift))
+            playerAnim.ResetTrigger("back");
+            if (!Input.anyKey)
+                playerAnim.SetTrigger("idle");
+            else
             {
-                walkSpeed += runSpeed;
-                playerAnim.SetTrigger("run");
-                playerAnim.ResetTrigger("walk");
-            }
+                playerAnim.ResetTrigger("idle");
 
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                walkSpeed = oldWalkSpeed;
-                playerAnim.ResetTrigger("run");
-                playerAnim.SetTrigger("walk");
+                if (Input.GetKey(KeyCode.A))
+                    playerAnim.SetTrigger("left");
+
+                if (Input.GetKey(KeyCode.D))
+                    playerAnim.SetTrigger("right");
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if(!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+                playerAnim.SetTrigger("left");
+
+            playerAnim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerAnim.ResetTrigger("left");
+            if (!Input.anyKey)
+                playerAnim.SetTrigger("idle");
+            else
+                playerAnim.ResetTrigger("idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+                playerAnim.SetTrigger("right");
+            playerAnim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerAnim.ResetTrigger("right");
+            if (!Input.anyKey)
+                playerAnim.SetTrigger("idle");
+            else
+                playerAnim.ResetTrigger("idle");
         }
     }
 }
